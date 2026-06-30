9ce8391ba5fbee1ba999c5ad48d3f0f4d894545e
Vinicius Pacheco <viniciusgrp@gmail.com>
2026-06-30 17:33:49 -0300

    fix: add validators to tenant register DTO

--- src/tenants/dto/register-tenant.dto.ts
+++ src/tenants/dto/register-tenant.dto.ts
@@ -1,7 +1,22 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
 export class RegisterTenantDto {
+  @IsString()
+  @IsNotEmpty()
   owner_name!: string;
+
+  @IsEmail()
   email!: string;
+
+  @IsString()
+  @MinLength(6)
   password!: string;
+
+  @IsString()
+  @IsNotEmpty()
   tenant_name!: string;
+
+  @IsString()
+  @IsNotEmpty()
   slug!: string;
 }
